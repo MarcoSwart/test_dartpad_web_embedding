0e8bca0af6e1ede1695670c0150bef453217a403
marco_swart <marco@nanosoft.co.za>
2021-06-02 09:48:16 +0200

    second commit

--- lib/dartpad/main.dart
+++ lib/dartpad/main.dart
@@ -1,1 +1,11 @@
-///
+import 'package:flutter/material.dart';
+import 'package:test_dartpad_web_embedding/main.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
